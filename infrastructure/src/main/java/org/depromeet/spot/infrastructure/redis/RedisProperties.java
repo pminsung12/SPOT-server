3a10bb1afa281ca5558c30e7d18bdfe3a11eda2c
Minseong Park <pminsung12@gmail.com>
2025-04-05 18:35:20 +0900

    fix: 쓰지 않는 코드 주석처리

--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/redis/RedisProperties.java
+++
@@ -1,6 +0,0 @@
-// package org.depromeet.spot.infrastructure.redis;
-//
-// import org.springframework.boot.context.properties.ConfigurationProperties;
-//
-// @ConfigurationProperties(prefix = "aws.redis")
-// public record RedisProperties(String host, int port) {}
