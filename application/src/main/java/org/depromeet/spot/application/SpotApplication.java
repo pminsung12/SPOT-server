b82fce78b9dca4fcc7c3796df9d1404c5f31e78b
Minseong Park <pminsung12@gmail.com>
2025-04-05 18:36:51 +0900

    feat: enable caching 어노테이션 추가

--- application/src/main/java/org/depromeet/spot/application/SpotApplication.java
+++ application/src/main/java/org/depromeet/spot/application/SpotApplication.java
@@ -6,7 +6,9 @@ import jakarta.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class SpotApplication {
 
--- infrastructure/src/main/java/org/depromeet/spot/infrastructure/InfrastructureConfig.java
+++ infrastructure/src/main/java/org/depromeet/spot/infrastructure/InfrastructureConfig.java
@@ -1,10 +1,11 @@
 package org.depromeet.spot.infrastructure;
 
+import org.depromeet.spot.infrastructure.jpa.oauth.config.OauthProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableConfigurationProperties
+@EnableConfigurationProperties(OauthProperties.class)
 @ConfigurationPropertiesScan(basePackages = {"org.depromeet.spot.infrastructure"})
 @ComponentScan(basePackages = {"org.depromeet.spot.infrastructure"})
 public class InfrastructureConfig {}
